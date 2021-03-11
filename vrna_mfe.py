6def61b0021ddaf8c57a406a9f953801f964262c
vaiteaopuu <vaiteaopuu@gmail.com>
2021-03-11 17:49:48 +0100

    test

--- vrna_mfe.py
+++ vrna_mfe.py
@@ -12,9 +12,13 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    len_seq = len(args.sequence)
-    vrna_struct, vrna_mfe = fold(args.sequence)
-    print(args.sequence, len_seq, vrna_struct, vrna_mfe, vrna_struct.count("("))
+    if args.sequence is not None:
+        sequence = args.sequence
+    else:
+        sequence = "".join([l.strip() for l in open(args.seq_file) if not l.startswith(">")]).replace("T", "U")
+    len_seq = len(sequence)
+    vrna_struct, vrna_mfe = fold(sequence)
+    print(sequence, len_seq, vrna_struct, vrna_mfe, vrna_struct.count("("))
 
 
 if __name__ == '__main__':
